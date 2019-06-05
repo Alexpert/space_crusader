d94581d1f70f854b8b5aabd03c70a698d081c54b
Killian Pareilleux <killian.pareilleux@etu.unilim.fr>
2019-06-05 13:41:11 +0200

    __ft_model_add_Action_Handler
    Ajout de move et turn dans Action Handler, afin de pouvoir tester le modele

---
+++ game/main/model/ActionHandlerPlayer.java
@@ -0,0 +1,79 @@
+package game.main.model;
+
+public class ActionHandlerPlayer extends AbstractActionHandler {
+
+	Entity entity;
+	
+	ActionHandlerPlayer(Entity e) {
+		this.entity = e;
+	}
+	@Override
+	public void wizz(Direction d) {
+		System.out.println("wizz direction"+d+"");
+
+	}
+
+	@Override
+	public void pop(Direction d) {
+		System.out.println("pop direction"+d+"");
+
+	}
+
+	@Override
+	public boolean jump(Direction d) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hit(Direction d) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean protect(Direction d) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean pick(Direction d) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean store() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean get() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean power() {
+		if(this.entity.getHealth() > 0) {
+			this.entity.setHealth(this.entity.getHealth() + 1);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean kamikaze() {
+		this.entity.setHealth(0);
+		return true;
+	}
+
+	@Override
+	public boolean egg() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
