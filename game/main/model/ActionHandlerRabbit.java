54a87211fa47ebfe3864bc9f15371ea7c6eb000d
GUIVARCH <alan.guivarch.98860@gmail.com>
2019-06-06 09:48:58 +0200

    __ft_rabbit_init

---
+++ game/main/model/ActionHandlerRabbit.java
@@ -0,0 +1,68 @@
+package game.main.model;
+
+public class ActionHandlerRabbit extends AbstractActionHandler {
+	
+	@Override
+	public void wizz(Direction d) {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public  void pop(Direction d){
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public  boolean jump(Direction d){
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public  boolean hit(Direction d){
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public  boolean protect(Direction d){
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public  boolean pick(Direction d){
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public  boolean store(){
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public  boolean get(){
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public  boolean power(){
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public  boolean kamikaze(){
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public  boolean egg(){
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
