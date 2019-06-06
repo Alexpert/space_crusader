508a6c5241f2cd74444c822a719f77aad483880f
GUIVARCH <alan.guivarch.98860@gmail.com>
2019-06-06 13:56:45 +0200

    wip

--- game/main/model/ActionHandlerAdultRabbit.java
+++ game/main/model/ActionHandlerAdultRabbit.java
@@ -5,78 +5,89 @@ public class ActionHandlerAdultRabbit extends AbstractActionHandler{
 	@Override
 	public void patient() {
 		// TODO Auto-generated method stub
-		
+		return;
 	}
 
 	@Override
 	public void wizz(Direction d) {
-		// TODO Auto-generated method stub
+		// TODO move faster
 		
 	}
 
 	@Override
 	public void pop(Direction d) {
-		// TODO Auto-generated method stub
+		// TODO laser
 		
 	}
 
 	@Override
 	public boolean jump(Direction d) {
-		// TODO Auto-generated method stub
+		// TODO don't jump
 		return false;
 	}
 
 	@Override
 	public boolean hit(Direction d) {
-		// TODO Auto-generated method stub
-		return false;
+		Tile t = this.entity.getTile(d);
+		if(t.isEmpty()) {
+			return false;
+		}
+		else {
+			for(int i = 0; i < t.nbEntity(); i ++) {
+				t.getEntity(i).addHealth(-10);
+			}
+			return true;
+		}
 	}
 
 	@Override
 	public boolean protect(Direction d) {
-		// TODO Auto-generated method stub
+		// TODO don't protect
 		return false;
 	}
 
 	@Override
 	public boolean pick(Direction d) {
-		// TODO Auto-generated method stub
+		// TODO don't pick
 		return false;
 	}
 
 	@Override
 	public boolean cast(Direction d) {
-		// TODO Auto-generated method stub
+		// TODO don't cast
 		return false;
 	}
 
 	@Override
 	public boolean store() {
-		// TODO Auto-generated method stub
+		// TODO don't store
 		return false;
 	}
 
 	@Override
 	public boolean get() {
-		// TODO Auto-generated method stub
+		// TODO don't get
 		return false;
 	}
 
 	@Override
 	public boolean power() {
-		// TODO Auto-generated method stub
+		if(this.entity.getHealth() > 0) {
+			this.entity.addHealth(1);
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean kamikaze() {
-		// TODO Auto-generated method stub
-		return false;
+		this.entity.addHealth(0);
+		return true;
 	}
 
 	@Override
 	public boolean egg() {
-		// TODO Auto-generated method stub
+		// TODO don't egg
 		return false;
 	}
 
--- game/main/model/Entity.java
+++ game/main/model/Entity.java
@@ -1,7 +1,7 @@
 package game.main.model;
 
 public abstract class Entity {
-	
+
 	private int health;
 	private int maxHealth;
 	private int x, y;
@@ -9,7 +9,7 @@ public abstract class Entity {
 	private boolean moveable;
 	private AbstractActionHandler actionHandler;
 	private World world;
-	
+
 	Entity(int x, int y, int health, Direction d, boolean moveable, World world) {
 		this.x = x;
 		this.y = y;
@@ -19,148 +19,237 @@ public abstract class Entity {
 		this.moveable = moveable;
 		this.world = world;
 	}
-	
+
 	public void setActionHandler(AbstractActionHandler ac) {
 		this.actionHandler = ac;
 	}
-	
+
 	public abstract void step();
-	
+
 	public int getX() {
 		return this.x;
 	}
-	
+
 	public void setX(int x) {
 		this.x = x;
 	}
-	
+
 	public int getY() {
 		return this.y;
 	}
-	
+
 	public void setY(int y) {
 		this.y = y;
 	}
-	
+
 	public int getHealth() {
 		return this.health;
 	}
-	
+
 	public void addHealth(int healthpoints) {
 		this.health += healthpoints;
 	}
-	
+
 	public Direction getOrientation() {
 		return this.orientation;
 	}
-	
+
 	public void setOrientation(Direction d) {
 		this.orientation = d;
 	}
-	
+
 	public boolean moveable() {
 		return this.moveable;
 	}
-	
+
 	protected AbstractActionHandler getActionHandler() {
 		return this.actionHandler;
 	}
-	
+
 	public World getWorld() {
 		return this.world;
 	}
-	
+
 	public Tile getTile(Direction d) {
 		Direction d2 = d;
-		if (d.ordinal() < 4 ) { //if the direction is not absolute
-			if(d == Direction.LEFT) {
-				d2 = d.get(((this.getOrientation().ordinal()+1)%4)+4);	//return WEST if the direction is NORTH
-			}
-			else if(d == Direction.RIGHT) {
-				d2 = d.get(((this.getOrientation().ordinal()+3)%4)+4);
-			}
-			else if(d == Direction.BACK) {
-				d2 = d.get(((this.getOrientation().ordinal()+2)%4)+4);
-			}
-			else if(d == Direction.FRONT) {
+		if (d.ordinal() < 4) { // if the direction is not absolute
+			if (d == Direction.LEFT) {
+				d2 = d.get(((this.getOrientation().ordinal() + 1) % 4) + 4); // return WEST if the direction is NORTH
+			} else if (d == Direction.RIGHT) {
+				d2 = d.get(((this.getOrientation().ordinal() + 3) % 4) + 4);
+			} else if (d == Direction.BACK) {
+				d2 = d.get(((this.getOrientation().ordinal() + 2) % 4) + 4);
+			} else if (d == Direction.FRONT) {
 				d2 = this.getOrientation();
 			}
 		}
-		
-		if(d2 == Direction.NORTH) {
-			if(this.getY() != 0) {
-				return this.getWorld().getTile(this.getX(), this.getY()-1);
-			}else {
-				return this.getWorld().getTile(this.getX(), this.getWorld().getHeight()-1);
+
+		if (d2 == Direction.NORTH) {
+			if (this.getY() != 0) {
+				return this.getWorld().getTile(this.getX(), this.getY() - 1);
+			} else {
+				return this.getWorld().getTile(this.getX(), this.getWorld().getHeight() - 1);
 			}
 		}
-		if(d2 == Direction.SOUTH) {
-			if(this.getY() != this.getWorld().getHeight()+1) {
-				return this.getWorld().getTile(this.getX(), this.getY()+1);
-			}else {
+		if (d2 == Direction.SOUTH) {
+			if (this.getY() != this.getWorld().getHeight() + 1) {
+				return this.getWorld().getTile(this.getX(), this.getY() + 1);
+			} else {
 				return this.getWorld().getTile(this.getX(), 0);
 			}
 		}
-		if(d2 == Direction.EAST) {
-			if(this.getX() != this.getWorld().getWidth()-1) {
-				return this.getWorld().getTile(this.getX()+1, this.getY());
-			}else {
+		if (d2 == Direction.EAST) {
+			if (this.getX() != this.getWorld().getWidth() - 1) {
+				return this.getWorld().getTile(this.getX() + 1, this.getY());
+			} else {
 				return this.getWorld().getTile(0, this.getY());
 			}
 		}
-		if(d2 == Direction.WEST) {
-			if(this.getY() != 0) {
-				return this.getWorld().getTile(this.getX()-1, this.getY());
-			}else {
-				return this.getWorld().getTile(this.getWorld().getWidth()-1, this.getY());
+		if (d2 == Direction.WEST) {
+			if (this.getY() != 0) {
+				return this.getWorld().getTile(this.getX() - 1, this.getY());
+			} else {
+				return this.getWorld().getTile(this.getWorld().getWidth() - 1, this.getY());
 			}
 		}
 		return null;
 	}
-	
-	
-	
+
 	public void move() {
 		this.getActionHandler().move();
 	}
-	
+
 	public void move(Direction d) {
 		this.getActionHandler().move(d);
 	}
-	
+
 	public void turn(Direction d) {
 		this.getActionHandler().turn(d);
 	}
-	
+
 	public void hit(Direction d) {
 		this.getActionHandler().hit(d);
 	}
-	
+
 	// Condition
-	
-	public boolean Key (String key) {
-		//TODO
+
+	public boolean Key(String key) {
+		// TODO
 		return false;
 	}
-	
-	public boolean MyDir (Direction d) {
-		//TODO
+
+	public boolean MyDir(Direction d) {
+		if (this.orientation == d) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean Cell(Direction d, Entity e, int distance) {
+		Direction d2 = d;
+		boolean res = false;
+		if (d.ordinal() < 4) { // if the direction is not absolute
+			if (d == Direction.LEFT) {
+				d2 = d.get(((this.getOrientation().ordinal() + 1) % 4) + 4); // return WEST if the direction is NORTH
+			} else if (d == Direction.RIGHT) {
+				d2 = d.get(((this.getOrientation().ordinal() + 3) % 4) + 4);
+			} else if (d == Direction.BACK) {
+				d2 = d.get(((this.getOrientation().ordinal() + 2) % 4) + 4);
+			} else if (d == Direction.FRONT) {
+				d2 = this.getOrientation();
+			}
+		}
+		if (d2 == Direction.NORTH) {
+			int i = this.x - distance + 1;
+			int j = this.y - 1;
+			while (i < this.x + distance - 1 && !res) {
+				Tile tile = this.world.getTile(i, j);
+				if (!tile.isEmpty()) {
+					for (int k = 0; k < tile.nbEntity(); k++) {
+						if (tile.getEntity(k).getClass() == e.getClass()) {
+							res = true;
+						}
+					}
+				}
+				if (i < this.x) {
+					j--;
+				} else {
+					j++;
+				}
+				i++;
+			}
+		}
+		if (d2 == Direction.SOUTH) {
+			int i = this.x - distance + 1;
+			int j = this.y + 1;
+			while (i < this.x + distance - 1 && !res) {
+				Tile tile = this.world.getTile(i, j);
+				if (!tile.isEmpty()) {
+					for (int k = 0; k < tile.nbEntity(); k++) {
+						if (tile.getEntity(k).getClass() == e.getClass()) {
+							res = true;
+						}
+					}
+				}
+				if (i < this.x) {
+					j++;
+				} else {
+					j--;
+				}
+				i++;
+			}
+		}
+		if (d2 == Direction.EAST) {
+			int i = this.x - 1;
+			int j = this.y - distance + 1;
+			while (i < this.y + distance - 1 && !res) {
+				Tile tile = this.world.getTile(i, j);
+				if (!tile.isEmpty()) {
+					for (int k = 0; k < tile.nbEntity(); k++) {
+						if (tile.getEntity(k).getClass() == e.getClass()) {
+							res = true;
+						}
+					}
+				}
+				if (j < this.y) {
+					i--;
+				} else {
+					i++;
+				}
+				j++;
+			}
+		}
+		if (d2 == Direction.WEST) {
+			int i = this.x + 1;
+			int j = this.y - distance + 1;
+			while (i < this.y + distance - 1 && !res) {
+				Tile tile = this.world.getTile(i, j);
+				if (!tile.isEmpty()) {
+					for (int k = 0; k < tile.nbEntity(); k++) {
+						if (tile.getEntity(k).getClass() == e.getClass()) {
+							res = true;
+						}
+					}
+				}
+				if (j < this.y) {
+					i++;
+				} else {
+					i--;
+				}
+				j++;
+			}
+		}
+		return res;
+	}
+
+	public boolean Closest(Entity e, Direction d) {
+		// TODO
 		return false;
 	}
-	
-	public boolean Cell (Direction d, Entity e) {
-		//TODO
+
+	public boolean GotStuff() {
 		return false;
 	}
-	
-	public boolean Closest (Entity e, Direction d) {
-		//TODO
-		return false;		
-	}
-	
-	public boolean GotStuff () {
-		//TODO
-		return false;
-	}
-	
+
 }
