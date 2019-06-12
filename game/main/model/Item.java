b0916ff06d68177eff1ef8b90fe4d24cd233cff0
Alexandre SALMON <salmon.alexandre10@gmail.com>
2019-06-12 09:21:22 +0200

    inventory: item, dropped item, usableitem

--- game/main/model/Item.java
+++ game/main/model/Item.java
@@ -1,4 +1,35 @@
 package game.main.model;
 
-public class Item {
+public abstract class Item {
+	private String name;
+	private int value = 0;
+	
+	protected Item(String name) {
+		this.setName(name);
+	}
+	
+	protected Item(String name, int value) {
+		this.setName(name);
+		this.setValue(value);
+	}
+	
+	public DroppedItem dropAtTile(Tile tile) {
+		return new DroppedItem(tile, this);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	private void setName(String name) {
+		this.name = name;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	private void setValue(int value) {
+		this.value = value;
+	}
 }
